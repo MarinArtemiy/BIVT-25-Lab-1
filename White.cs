e6a91e3708325e96a1bf5928eab11128c364b125
MarinArtemiy <m2512392@edu.misis.ru>
2025-09-05 10:14:02 +0300

    Add files via upload

---
+++ White.cs
@@ -0,0 +1,105 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+
+namespace Lab1
+{
+    public class White
+    {
+        public bool Task1(double d)
+        {
+            bool answer = false;
+
+            // code here
+            if (d > 0)
+            {
+                answer = true;
+            } 
+            else
+            {
+                answer = false;
+            }
+            // end
+
+            return answer;
+        }
+        public bool Task2(int n)
+        {
+            bool answer = false;
+
+            // code here
+            if (n%2 == 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+            // end
+
+            return answer;
+        }
+        public int Task3(int a, int b)
+        {
+            int answer = 0;
+
+            // code here
+            answer = Math.Max(a, b);
+            Console.WriteLine("true");
+            // end
+
+            return answer;
+        }
+        public double Task4(double d, double f)
+        {
+            double answer = 0;
+
+            // code here
+
+            // end
+
+            return answer;
+        }
+        public double Task5(double x)
+        {
+            double answer = 0;
+
+            // code here
+
+            // end
+
+            return answer;
+        }
+        public bool Task6(double x, double y, double r)
+        {
+            bool answer = false;
+
+            // code here
+
+            // end
+
+            return answer;
+        }
+
+        public bool Task7(int n)
+        {
+            bool answer = false;
+
+            // code here
+
+            // end
+
+            return answer;
+        }
+        public bool Task8(double L, int T, int M)
+        {
+            bool answer = false;
+
+            // code here
+
+            // end
+
+            return answer;
+        }
+    }
+}
